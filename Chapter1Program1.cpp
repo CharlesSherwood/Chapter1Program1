51f697b6b2e0ed2a81ff51357334d8bc60bb6c91
cks45004 <cks45004@HT-0232-13>
2025-01-24 10:09:39 -0500

    Add project files.

---
+++ Chapter1Program1.cpp
@@ -0,0 +1,18 @@
+/*
+ ChapterProgram1.cpp
+ Programmer:Charles Sherwood
+ Date:01/25
+ Requirements:
+Take the employees hours worked for the week and 
+payrate and compute his grosspay and display
+it on the monitor.
+*/
+
+#include <iostream>
+
+int main()
+{
+
+
+
+}
